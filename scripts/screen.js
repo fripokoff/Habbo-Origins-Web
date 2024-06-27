7db10886da4571e4d23954c3973ea0142bac27ae
Fripok <azceus@gmail.com>
2024-06-27 02:09:33 +0200

    Console furni values, add all server , style page

--- scripts/screen.js
+++ scripts/screen.js
@@ -1,43 +1,70 @@
-function insertObjectWithScreenSize() {
-				const screenWidth = window.innerWidth;
-				const screenHeight = window.innerHeight;
-            const objectHTML = `
-                <object classid="clsid:166B1BCA-3F9C-11CF-8075-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/director/sw.cab#version=10,8,5,1,0" id="habbo" width="${screenWidth}" height="${screenHeight}">
-                    <param name="src" value="dcr/habbo.dcr">
-                    <param name="swStretchStyle" value="meet">
-                    <param name="bgColor" value="#000000">
-                    <param name="sw2" value="connection.info.host=game-ous.habbo.com;connection.info.port=40001">
-					<param name="sw3" value="connection.mus.host=game-ous.habbo.com;connection.mus.port=40002">
-					<param name="sw4" value="site.url=http://origins.habbo.com;url.prefix=http://origins.habbo.com/">
-					<param name="sw5" value="client.reload.url=http://origins.habbo.com/habbo/br;client.fatal.error.url=http://origins.habbo.com/">
-					<param name="sw6" value="external.variables.txt=http://origins-gamedata.habbo.com/external_variables/1">
-					<param name="sw7" value="external.texts.txt=http://origins-gamedata.habbo.com/external_texts/1">
-					<param name="sw8" value="client.allow.cross.domain=1;client.notify.cross.domain=0">
-                    <embed id="habboEmbed" src="dcr/habbo.dcr" bgcolor="#000000" width="${screenWidth}" height="${screenHeight}" swvolume="true"
-                    swrestart="false" swpauseplay="false" swfastforward="false" swtitle="Habbo Hotel" swcontextmenu="true"
-                    swstretchstyle="meet" swtext="" type="application/x-director" pluginspage="http://www.macromedia.com/shockwave/download/"
-                    sw2="connection.info.host=game-ous.habbo.com;connection.info.port=40001"
-                    sw3="connection.mus.host=game-ous.habbo.com;connection.mus.port=40002"
-                    sw4="site.url=http://origins.habbo.com;url.prefix=http://origins.habbo.com/"
-                    sw5="client.reload.url=http://origins.habbo.com/habbo/br;client.fatal.error.url=http://origins.habbo.com/"
-                    sw6="external.variables.txt=http://origins-gamedata.habbo.com/external_variables/1"
-                    sw7="external.texts.txt=http://origins-gamedata.habbo.com/external_texts/1"
-                    sw8="client.allow.cross.domain=1;client.notify.cross.domain=0"></embed>
-                </object>
-            `;
-            document.getElementById('container').innerHTML = objectHTML;
-        }
-		function updateEmbedSize() {
-			const screenWidth = window.innerWidth;
-			const screenHeight = window.innerHeight;
-			console.log(screenHeight);
-			const embed = document.getElementById('habboEmbed');
-			if (embed) {
-				embed.width = screenWidth;
-				embed.height = screenHeight;
-			}
+var habboEmbed = "";
+
+function updateEmbedSize() {
+	const screenWidth = window.innerWidth;
+	const screenHeight = window.innerHeight;
+	console.log(screenHeight);
+	const embed = document.getElementById('habboEmbed');
+	const fixedRight = document.getElementById('fixedRight');
+	const main_console = document.getElementById('main-console');
+
+	if (embed) {
+		embed.style.width = screenWidth + 'px';
+		embed.style.height = screenHeight + 'px';
+	}
+
+	if (fixedRight) {
+		fixedRight.style.width = (screenWidth * 0.165) + 'px';
+		fixedRight.style.height = screenHeight + 'px';
+	}
+	if (main_console) {
+		main_console.style.width = (screenWidth * 0.157) + 'px';
+		main_console.style.height = screenHeight + 'px';
+	}
+}
+
+window.addEventListener('resize', function() {
+	updateEmbedSize();
+});
+
+updateEmbedSize();
+
+document.getElementById('fullscreenBtn').addEventListener('click', function() {
+	const elem = document.documentElement;
+
+	const isFullscreen = document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement;
+
+	if (!isFullscreen) {
+		if (elem.requestFullscreen) {
+			elem.requestFullscreen();
+		} else if (elem.mozRequestFullScreen) {
+			/* Firefox */
+			elem.mozRequestFullScreen();
+		} else if (elem.webkitRequestFullscreen) {
+			/* Chrome, Safari & Opera */
+			elem.webkitRequestFullscreen();
+		} else if (elem.msRequestFullscreen) {
+			/* IE/Edge */
+			elem.msRequestFullscreen();
 		}
-			window.addEventListener('resize', function() {
-				updateEmbedSize();
-	});
-        window.addEventListener('load', insertObjectWithScreenSize);
+	} else { // Sortir du plein écran
+		if (document.exitFullscreen) {
+			document.exitFullscreen();
+		} else if (document.mozCancelFullScreen) {
+			/* Firefox */
+			document.mozCancelFullScreen();
+		} else if (document.webkitExitFullscreen) {
+			/* Chrome, Safari & Opera */
+			document.webkitExitFullscreen();
+		} else if (document.msExitFullscreen) {
+			/* IE/Edge */
+			document.msExitFullscreen();
+		}
+	}
+});
+
+if (!localStorage.getItem('origins_hotel')) {
+	localStorage.setItem('origins_hotel', 'us');
+}
+
+setLoaderbyCouuntryCode(countryCode);
---
+++ scripts/values.js
@@ -0,0 +1,98 @@
+var html = "";
+
+function extractValuesFromHTML(html) {
+	const parser = new DOMParser();
+	const doc = parser.parseFromString(html, 'text/html');
+	const items = doc.querySelectorAll('.grid-item');
+	const extractedValues = [];
+
+	items.forEach(item => {
+		const name = item.querySelector('h2').innerText;
+		const colaValue = item.querySelector('.cola-value') ? item.querySelector('.cola-value').innerText : 'N/A';
+		const hcValue = item.querySelector('.hc-value') ? item.querySelector('.hc-value').innerText : 'N/A';
+		const images = item.querySelectorAll('img');
+		const imageUrls = Array.from(images).map(img => img.src.replace('http://localhost:3000', ''));
+
+		extractedValues.push({
+			name,
+			colaValue,
+			hcValue,
+			images: imageUrls[0]
+		});
+	});
+
+	return extractedValues;
+}
+
+function getOriginValues() {
+	fetch('https://originvalues.com/')
+		.then(response => response.text())
+		.then(html => {
+			const parser = new DOMParser();
+			const doc = parser.parseFromString(html, 'text/html');
+			const xpath1 = '/html/body/table/tbody/tr/td/table[3]/tbody/tr/td/table[2]/tbody/tr/td[3]/table/tbody/tr/td/div[1]';
+			const xpath2 = '/html/body/table/tbody/tr/td/table[3]/tbody/tr/td/table[2]/tbody/tr/td[3]/table/tbody/tr/td/div[2]';
+
+			function extractContentByXPath(doc, xpath) {
+				if (doc instanceof Document && doc.evaluate) {
+					const result = doc.evaluate(xpath, doc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+					return result.singleNodeValue ? result.singleNodeValue.innerHTML : 'Contenu non trouvé';
+				} else {
+					console.error("L'objet 'doc' n'est pas un document valide.");
+					return 'Contenu non trouvé';
+				}
+			}
+			let rares = extractContentByXPath(doc, xpath1);
+			let club = extractContentByXPath(doc, xpath2);
+
+			rares = extractValuesFromHTML(rares);
+			club = extractValuesFromHTML(club);
+			setTimeout(() => {
+				document.getElementById('loader').style.display = 'none';
+				appendItems(rares)
+				appendItems(club)
+			}, 1000);
+
+		})
+		.catch(error => console.error('Erreur lors de la récupération des données:', error));
+
+}
+
+function appendItems(items) {
+	const container = document.getElementById('items');
+	container.innerHTML = '';
+
+	items.forEach(item => {
+		const div = document.createElement('div');
+		div.className = 'chat';
+		div.innerHTML = `
+	  <div class="row">
+	  <div class="furni_icon main-box">
+		<img class="big-furni" src="https://originvalues.com/${item.images}"/>
+	  </div>
+	  <div class="dialog left">
+		<div class="row">
+		  <div class="furni_icon">
+			<img class="furni_icon" src="https://originvalues.com/imgs/furni/habbo_cola.png"/>
+		  </div>
+		  <div class="warning__title">${item.colaValue}</div>
+		</div>
+		<div class="row">
+		  <div class="furni_icon">
+			<img class="furni_icon" src="https://originvalues.com/imgs/hc_icon.png"/>
+		  </div>
+		  <div class="warning__title">${item.hcValue}</div>
+		</div>
+	  </div>
+	  </div>`;
+		container.appendChild(div);
+	});
+}
+
+document.getElementById('refreshBtn').addEventListener('click', function() {
+	document.getElementById('loader').style.display = 'block';
+	document.getElementById('items').innerHTML = '';
+	getOriginValues();
+});
+
+getOriginValues();
